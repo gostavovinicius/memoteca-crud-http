16b0c8bb750887d2007e7cf2b17fc7e11d56da22
Gustavo Vinícius <88352305+gostavovinicius@users.noreply.github.com>
2025-03-24 09:04:58 -0300

    Requisição PUT

--- js/main.js
+++ js/main.js
@@ -5,7 +5,9 @@ document.addEventListener("DOMContentLoaded", () => {
     ui.renderizarPensamentos()
 
     const formularioPensamento = document.getElementById('pensamento-form')
+    const botaoCancelar = document.getElementById('botao-cancelar')
     formularioPensamento.addEventListener("submit", manipularSubmissaoFormulario)
+    botaoCancelar.addEventListener("click", manipularCancelamento)
 })
 
 async function manipularSubmissaoFormulario(event) {
@@ -15,9 +17,17 @@ async function manipularSubmissaoFormulario(event) {
     const autoria = document.getElementById("pensamento-autoria").value
 
     try {
-        await api.salvarPensamento({ conteudo, autoria})
+        if (id) {
+            await api.editarPensamento({ id, conteudo, autoria })
+        } else {
+        await api.salvarPensamento({ conteudo, autoria })
+        }
         ui.renderizarPensamentos()
     } catch {
        alert('Erro ao salvar o pensamento!') 
     }
+}
+
+function manipularCancelamento() {
+    ui.limparFormulario()
 }
