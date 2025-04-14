301b0cf1bce44a3fde65555098803f9805294e1a
Gustavo Vinícius <88352305+gostavovinicius@users.noreply.github.com>
2025-04-14 09:11:17 -0300

    Favoritando um pensamento

--- js/ui.js
+++ js/ui.js
@@ -25,7 +25,7 @@ const ui = {
       } else {
         pensamentosParaRenderizar = await api.buscarPensamentos()
       }
-      
+
       if (pensamentosParaRenderizar.length === 0) {
         mensagemVazia.style.display = "block";
       } else {
@@ -82,8 +82,27 @@ const ui = {
     iconeExcluir.alt = "Excluir"
     botaoExcluir.appendChild(iconeExcluir)
 
+    const botaoFavorito = document.createElement("button")
+    botaoFavorito.classList.add("botao-favorito")
+    botaoFavorito.onclick = async () => {
+      try {
+        await api.atualizarFavorito(pensamento.id, !pensamento.favorito)
+        ui.renderizarPensamentos()
+      } catch (error) {
+        alert("Erro ao atualizar o pensamento!")
+      }
+    }
+
+    const iconeFavorito = document.createElement("img")
+    iconeFavorito.src = pensamento.favorito ? 
+    "assets/imagens/icone-favorito.png" : 
+    "assets/imagens/icone-favorito_outline.png"
+    iconeFavorito.alt = "Icone de favorito"
+    botaoFavorito.appendChild(iconeFavorito)
+
     const icones = document.createElement("div")
     icones.classList.add("icones")
+    icones.appendChild(botaoFavorito)
     icones.appendChild(botaoEditar)
     icones.appendChild(botaoExcluir)
 
